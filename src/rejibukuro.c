ca5f7a7ac70564674d884d4b59d8e7c7b3419446
Prashant-Basnet-TP <282407901+Prashant-Basnet-TP@users.noreply.github.com>
2026-05-19 11:01:00 +0900

    edited

--- src/rejibukuro.c
+++ src/rejibukuro.c
@@ -2,12 +2,20 @@
 
 int main(void) {
     int rezibukuro;
+    int choice;
     printf("レジ袋を使用しますか？\n");
     printf("1. はい\n");
     printf("2. いいえ\n");
+    scanf("%d", &choice);
     getchar();
-    printf("何個必要ですか？1個当たり5円です。\n");
-    scanf("%d", &rezibukuro);
-    printf("%d個のレジ袋をカートに入れました。\n", rezibukuro);
+    if (choice == 1){
+         printf("何個必要ですか？1個当たり5円です。\n");
+        scanf("%d", &rezibukuro);
+        printf("%d個のレジ袋をカートに入れました。\n", rezibukuro);
+    }else if (choice == 2){
+        printf("レジ袋は使用しません。\n");
+        printf("このまま買い物を続けます。\n");
+    }
+   
     return 0;
 }
