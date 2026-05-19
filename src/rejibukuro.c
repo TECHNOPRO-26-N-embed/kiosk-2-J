f9128eb9caf5ae6d98ce852a3a1b4d0383d0b74a
Prashant-Basnet-TP <282407901+Prashant-Basnet-TP@users.noreply.github.com>
2026-05-19 10:37:01 +0900

    reji

--- src/rejibukuro.c
+++ src/rejibukuro.c
@@ -3,5 +3,11 @@
 int main(void) {
     int rezibukuro;
     printf("レジ袋を使用しますか？\n");
+    printf("1. はい\n");
+    printf("2. いいえ\n");
+    getchar();
+    printf("何個必要ですか？1個当たり5円です。\n");
     scanf("%d", &rezibukuro);
+    printf("%d個のレジ袋をカートに入れました。\n", rezibukuro);
+    return 0;
 }
